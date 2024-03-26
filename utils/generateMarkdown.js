d5903ff5d25091e8350ddfe6ac742bd9b8aecc84
Pema Sherpa <pematamang111@gmail.com>
2024-03-26 19:40:06 -0400

    add data.name

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -32,7 +32,31 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  by ${data.name}
+  ${renderLicenseBadge(data.license)}
+  ## Table of Contents
+  * [Description](#desription)
+  * [Requirements](#requirements)
+  * [Usage](#Usage)
+  * [Contact-Me](#contact- me)
+  * [Contributors](#ontributors)
+  * [Testing](#testing)
+  ${renderLicenseLink(data.license)}
+  ## Description
+  ${data.description}
+  ## Requirements
+  ${data.require}
+  ## Contact-Me
+  * Name - ${data.name}
+  * Email - ${data.email}
+  * Github - [${data.creator}](https://github.com/${data.creator});
+  ## Contributors
+  ${data.contributors}
+  ## Testing
+  \`\`\`
+  ${data.test}
+  \`\`\`
+  ${renderLicenseSection(data.license)}
 `;
 }
 
